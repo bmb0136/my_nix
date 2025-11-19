bfafa655b445ddc50d43f8e1415bf133b991dd3c
Brandon Buckley <brandonmb0136@gmail.com>
2025-11-19 13:48:20 -0600

    Add missing url for HM

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
