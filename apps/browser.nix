0588807760a39dd2d0e2e4fa4d90a766b3f879ec
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-26 21:04:57 -0500

    Use ungoogled chrome instead of firefox

--- apps/browser.nix
+++ apps/browser.nix
@@ -1,6 +1,47 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    firefox
+  home-manager.sharedModules = [
+    {
+      programs.chromium = {
+        enable = true;
+        package = pkgs.ungoogled-chromium;
+        extensions =
+          let
+            createChromiumExtensionFor =
+              browserVersion:
+              {
+                id,
+                sha256,
+                version,
+              }:
+              {
+                inherit id;
+                crxPath = builtins.fetchurl {
+                  url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${browserVersion}&x=id%3D${id}%26installsource%3Dondemand%26uc";
+                  name = "${id}.crx";
+                  inherit sha256;
+                };
+                inherit version;
+              };
+            createChromiumExtension = createChromiumExtensionFor (
+              lib.versions.major pkgs.ungoogled-chromium.version
+            );
+          in
+          map createChromiumExtension [
+            # UBO
+            {
+              id = "ddkjiahejlhfcafbddmgiahcphecmpfh";
+              sha256 = "sha256:0swqw3siwgpvqvqb1hrmibs2snpl10sdhy7qdc8xja87qyjjmwqz";
+              version = "2025.1217.1755";
+            }
+            # Dark Reader
+            {
+              id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";
+              sha256 = "sha256:0hijj2gd34qqspxx9y7kjdya5q9kdjl4lajc6mgh7jysziw0c838";
+              version = "4.9.118";
+            }
+          ];
+      };
+    }
   ];
 }
