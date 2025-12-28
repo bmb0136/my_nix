4fa5d60bae7d6fb7dfb52cc8cfbe6837e228c86c
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-28 16:51:30 -0500

    Add mkvim command

--- apps/basic-tools.nix
+++ apps/basic-tools.nix
@@ -1,13 +1,25 @@
-{ pkgs, ... }: {
+{ inputs, pkgs, ... }:
+{
   environment.shellAliases = {
     l = "ls -lah";
     ll = "ls -l";
     ls = "ls --color=tty";
     cls = "clear";
-    htop = "btop";
   };
 
   environment.systemPackages = [
     pkgs.btop
+    pkgs.htop
+    pkgs.wget
+    pkgs.curl
+
+    (pkgs.writeShellApplication {
+      name = "mkvim";
+      text = ''
+        lang=$1
+        shift
+        nix run "${inputs.my_nvim}#$lang" -- "$@"
+      '';
+    })
   ];
 }
