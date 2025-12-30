d1a0dab0dd2eee9f2a719b6a7fce67e87a5e0e7e
Brandon Buckley <brandonmb0136@gmail.com>
2025-12-30 01:01:38 -0500

    Add avante to nvim if ollama is present

--- apps/nix.nix
+++ apps/nix.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ inputs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix = {
     settings = {
       auto-optimise-store = true;
--- apps/nvim/ollama.nix
+++ apps/nvim/ollama.nix
@@ -4,6 +4,7 @@
     setupOpts = {
       provider = "ollama";
       providers.ollama = {
+        model = "gpt-oss:20b";
         endpoint = "http://127.0.0.1:11434";
         timeout = 15000;
       };
