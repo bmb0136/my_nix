96e652f57b854e3da25f8876699d877dbf8cf9a2
Brandon Buckley <brandonmb0136@gmail.com>
2026-01-03 23:10:13 -0500

    Fix twemoji

--- theme/fonts.nix
+++ theme/fonts.nix
@@ -1,27 +1,63 @@
 { pkgs, config, ... }:
 {
   config = {
-    stylix.fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font";
-      };
-      emoji = {
-        name = "Twitter Color Emoji";
-        package = pkgs.symlinkJoin {
-          name = "twemoji";
-          paths = with pkgs; [
-            twemoji-color-font
-            twitter-color-emoji
-          ];
+    stylix = {
+      fonts = {
+        monospace = {
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrains Mono Nerd Font";
+        };
+        emoji = {
+          package = pkgs.stdenv.mkDerivation {
+            name = "twemoji-hack";
+            installPhase = ''
+              mkdir -p $out/share/fonts/truetype
+              cp "${pkgs.twitter-color-emoji}/share/fonts/truetype/TwitterColorEmoji.ttf" "$out/share/fonts/truetype/TwitterColorEmoji-SVGinOT.ttf"
+            '';
+            dontUnpack = true;
+          };
+          name = "Twitter Color Emoji";
         };
       };
     };
-    fonts.fontconfig = {
-      enable = true;
-      defaultFonts = {
-        monospace = [ config.stylix.fonts.monospace.name ];
-        emoji = [ config.stylix.fonts.emoji.name ];
+
+    fonts = {
+      fontconfig = {
+        enable = true;
+        #useEmbeddedBitmaps = true;
+        localConf = ''
+          <?xml version='1.0'?>
+          <!DOCTYPE fontconfig SYSTEM 'urn:fontconfig:fonts.dtd'>
+          <fontconfig>
+            <alias binding="same">
+              <family>sans-serif</family>
+              <prefer>
+                <family>${config.stylix.fonts.sansSerif.name}</family>
+                <family>${config.stylix.fonts.emoji.name}</family>
+              </prefer>
+            </alias>
+            <alias binding="same">
+              <family>serif</family>
+              <prefer>
+                <family>${config.stylix.fonts.serif.name}</family>
+                <family>${config.stylix.fonts.emoji.name}</family>
+              </prefer>
+            </alias>
+            <alias binding="same">
+              <family>monospace</family>
+              <prefer>
+                <family>${config.stylix.fonts.monospace.name}</family>
+                <family>${config.stylix.fonts.emoji.name}</family>
+              </prefer>
+            </alias>
+            <alias binding="same">
+              <family>emoji</family>
+              <prefer>
+                <family>${config.stylix.fonts.emoji.name}</family>
+              </prefer>
+            </alias>
+          </fontconfig>
+        '';
       };
     };
   };
