12821b6a5bb8121765b28e5d23e4e8a95f51cfbe
Brandon Buckley <brandonmb0136@gmail.com>
2026-01-04 18:35:55 -0600

    Use cleaner syntax for lang nvims

--- apps/nvim/default.nix
+++ apps/nvim/default.nix
@@ -19,49 +19,44 @@ in
   config.environment.systemPackages = [
     (my_nvim.default.override { inherit modules; })
   ]
-  ++ (
-    if config.bmb0136.nvim.add-lang-nvims then
-      (
-        let
-          nvim-c = my_nvim.c.override { inherit modules; };
-          nvim-cs = my_nvim.csharp.override {
-            inherit modules;
-            enable-fsharp = true;
-            enable-html = true;
-            enable-sql = true;
-          };
-          nvim-js = my_nvim.ts.override {
-            inherit modules;
-            enable-svelte = true;
-            enable-tailwind = true;
-          };
+  ++ lib.optionals config.bmb0136.nvim.add-lang-nvims (
+    let
+      nvim-c = my_nvim.c.override { inherit modules; };
+      nvim-cs = my_nvim.csharp.override {
+        inherit modules;
+        enable-fsharp = true;
+        enable-html = true;
+        enable-sql = true;
+      };
+      nvim-js = my_nvim.ts.override {
+        inherit modules;
+        enable-svelte = true;
+        enable-tailwind = true;
+      };
 
-          nvims = {
-            inherit nvim-c;
-            nvim-cpp = nvim-c;
+      nvims = {
+        inherit nvim-c;
+        nvim-cpp = nvim-c;
 
-            nvim-rust = my_nvim.rust.override {
-              inherit modules;
-              enable-c = true;
-            };
+        nvim-rust = my_nvim.rust.override {
+          inherit modules;
+          enable-c = true;
+        };
 
-            nvim-py = my_nvim.python.override { inherit modules; };
+        nvim-py = my_nvim.python.override { inherit modules; };
 
-            inherit nvim-cs;
-            nvim-fs = nvim-cs;
+        inherit nvim-cs;
+        nvim-fs = nvim-cs;
 
-            inherit nvim-js;
-            nvim-ts = nvim-js;
-          };
-          replaceName =
-            name: val:
-            pkgs.writeShellScriptBin name ''
-              "${val}/bin/nvim" "$@"
-            '';
-        in
-        lib.mapAttrsToList replaceName nvims
-      )
-    else
-      [ ]
+        inherit nvim-js;
+        nvim-ts = nvim-js;
+      };
+      replaceName =
+        name: val:
+        pkgs.writeShellScriptBin name ''
+          "${val}/bin/nvim" "$@"
+        '';
+    in
+    (lib.mapAttrsToList replaceName nvims)
   );
 }
