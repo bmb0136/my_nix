bba510d74252bb51b4c05fe2d45d7202870a8619
Brandon Buckley <brandonmb0136@gmail.com>
2026-01-16 19:47:42 -0600

    delete global shortcuts from config

--- apps/browser/default.nix
+++ apps/browser/default.nix
@@ -8,22 +8,53 @@
       programs.chromium = {
         enable = true;
         package = pkgs.ungoogled-chromium.override { enableWideVine = true; };
-        extensions = lib.pipe ./extensions.json [
-          builtins.readFile
-          builtins.fromJSON
-          (map (
-            { id, name }:
-            {
-              inherit id;
-              crxPath = ./crx/${name};
-              version = lib.pipe ./versions.json [
-                builtins.readFile
-                builtins.fromJSON
-                (builtins.getAttr id)
-              ];
-            }
-          ))
-        ];
+        extensions =
+          let
+            versions = lib.pipe ./versions.json [
+              builtins.readFile
+              builtins.fromJSON
+            ];
+
+            removeCommands =
+              { id, path }:
+              pkgs.stdenvNoCC.mkDerivation {
+                name = "${id}-no-commands";
+                dontUnpack = true;
+                nativeBuildInputs = with pkgs; [
+                  unzip
+                  zip
+                  jq
+                ];
+                buildPhase = ''
+                  cp "${path}" input.zip
+                  zip -FF input.zip --out temp.zip
+                  unzip -p temp.zip manifest.json | jq '.commands = {}' > manifest.json
+                  zip -u temp.zip manifest.json
+                  mkdir -p "$out"
+                  cp temp.zip "$out/extension.crx"
+                '';
+              };
+
+            mkExtension =
+              { id, name }:
+              {
+                inherit id;
+                crxPath =
+                  let
+                    drv = removeCommands {
+                      inherit id;
+                      path = ./crx/${name};
+                    };
+                  in
+                  "${drv}/extension.crx";
+                version = builtins.getAttr id versions;
+              };
+          in
+          lib.pipe ./extensions.json [
+            builtins.readFile
+            builtins.fromJSON
+            (map mkExtension)
+          ];
       };
     }
   ];
