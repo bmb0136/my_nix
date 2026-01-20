26b965dc1caa9d3efbb05b7e32b8da19e6a8a203
Brandon Buckley <brandonmb0136@gmail.com>
2026-01-19 19:22:47 -0600

    Add printing + libreoffice to desktop bundle

--- apps/bundles/desktop.nix
+++ apps/bundles/desktop.nix
@@ -10,7 +10,15 @@
 
   environment.systemPackages = [
     pkgs.vlc
+    pkgs.libreoffice
   ];
+
+  services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   services.flatpak.enable = true;
 }
