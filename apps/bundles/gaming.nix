17a1af2c778da8fd9bbf07d58ce48294e7f3e801
Brandon Buckley <brandonmb0136@gmail.com>
2026-02-10 21:19:37 -0600

    fix OBS virtual cam

--- apps/bundles/gaming.nix
+++ apps/bundles/gaming.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   programs.steam.enable = true;
   home-manager.sharedModules = [
@@ -12,4 +13,13 @@
       }
     )
   ];
+
+  # OBS Virtual Camera
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
 }
