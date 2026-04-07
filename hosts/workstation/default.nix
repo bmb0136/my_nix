6fcc1e074f93d658eaf2ea0d233fb8822fa8bf79
Brandon Buckley <brandonmb0136@gmail.com>
2026-04-07 18:28:39 -0500

    add ssh to workstation

--- hosts/workstation/default.nix
+++ hosts/workstation/default.nix
@@ -60,6 +60,11 @@
       };
     };
 
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
