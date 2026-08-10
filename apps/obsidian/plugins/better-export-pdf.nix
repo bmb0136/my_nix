0794259670e8861c6a6622725f6cfb2719b272ec
Brandon Buckley <brandonmb0136@gmail.com>
2026-08-10 11:06:45 -0500

    Update obsidian plugins

--- apps/obsidian/plugins/better-export-pdf.nix
+++ apps/obsidian/plugins/better-export-pdf.nix
@@ -4,11 +4,11 @@
 }:
 pkgs.stdenvNoCC.mkDerivation rec {
   name = "obsidian-better-export-pdf";
-  version = "1.11.0";
+  version = "2.0.3";
 
   srcs = pkgs.fetchzip {
     url = "https://github.com/l1xnan/${name}/releases/download/${version}/${name}.zip";
-    hash = "sha256-9q5gHFJsMXnS+tskkmXaUzoNvWywtqpnboGEff4vfPE=";
+    hash = "sha256-3K7oxMRn7DC9fjggIuTzjFzr4D2OzDpnyyXuxXi1WN8=";
   };
 
   buildPhase = ''
--- apps/obsidian/plugins/obsidian-git.nix
+++ apps/obsidian/plugins/obsidian-git.nix
@@ -4,11 +4,11 @@
 }:
 pkgs.stdenvNoCC.mkDerivation rec {
   name = "obsidian-git";
-  version = "2.36.0";
+  version = "2.38.6";
 
   srcs = pkgs.fetchzip {
     url = "https://github.com/Vinzent03/${name}/releases/download/${version}/${name}-${version}.zip";
-    hash = "sha256-6yKsgQ5HZNZWQscK/1Xm5s6MQe5/9INidju3AqxVoVo=";
+    hash = "sha256-GHaYFW9IL/T4dCxJLZ6A5Y6eQ4h+4aIJs095+/rxdic=";
   };
 
   buildPhase = ''
