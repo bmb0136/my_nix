a4572899d0844b0739bfda54ca2fa2ef028fe3ce
Brandon Buckley <brandonmb0136@gmail.com>
2026-08-20 16:27:15 -0500

    fixing bluetooth attempt 1

--- hosts/hp-laptop/hardware-configuration.nix
+++ hosts/hp-laptop/hardware-configuration.nix
@@ -40,6 +40,15 @@
 
   swapDevices = [ ];
 
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        ControllerMode = "bredr";
+      };
+    };
+  };
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
